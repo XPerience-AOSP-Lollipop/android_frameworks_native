69607291e2f7cab07506379acaf2ed8309a60ea4
Isaac Chen <ycchen@google.com>
2017-02-16 11:51:51 +0800

    Removed build time variable ENABLE_CPUSETS.
    Use runtime check, cpuset_enabled(), instead of build time varianble
    usually set in BoardConfog.mk.
    
    Bug: 34726944
    
    Test: Tested on Sailfish
          without cpuset mounted
          with cpuset mounted, but without schedtune
          with cpuset mounted and with schedtune
    
    Change-Id: I00153628db62abfbaa0ba75d571ba1bda0611ff3

--- services/surfaceflinger/main_surfaceflinger.cpp
+++ services/surfaceflinger/main_surfaceflinger.cpp
@@ -44,12 +44,10 @@ int main(int, char**) {
 
     setpriority(PRIO_PROCESS, 0, PRIORITY_REALTIME);
 
-#ifdef ENABLE_CPUSETS
     // Put most SurfaceFlinger threads in the system-background cpuset
     // Keeps us from unnecessarily using big cores
     // Do this after the binder thread pool init
-    set_cpuset_policy(0, SP_SYSTEM);
-#endif
+    if (cpusets_enabled()) set_cpuset_policy(0, SP_SYSTEM);
 
     // initialize before clients can connect
     flinger->init();
